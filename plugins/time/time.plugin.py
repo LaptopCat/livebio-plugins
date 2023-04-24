ac15192278c5ab5886b5e06c11beffbbeef6c7a6
Laptop <98688203+LaptopCat@users.noreply.github.com>
2023-04-24 20:38:26 +0300

    Update time.plugin.py

--- plugins/time/time.plugin.py
+++ plugins/time/time.plugin.py
@@ -1,9 +1,12 @@
 # livebio-tg plugin
-__plugin__ = {"name": "time", "author": "LaptopCat", "version": "1.0", "link": "https://github.com/LaptopCat/livebio-plugins/blob/main/plugins/time/README.md"} # plugin manifest
+__plugin__ = {"name": "time", "author": "LaptopCat", "version": "1.1", "link": "https://github.com/LaptopCat/livebio-plugins/blob/main/plugins/time/README.md"} # plugin manifest
 from datetime import datetime
 from pytz import timezone
 from config import Config
 time = Config.plugins.time
+time.time_formatting = getattr(time, "time_formatting", "%H:%M")
+time.postprocess = getattr(time, "postprocess", False)
+time.timezone = getattr(time, "timezone", None)
 tz = timezone(time.timezone)
 
 async def gather(): # This function is called to give back the string that needs to be added
@@ -11,7 +14,7 @@ async def gather(): # This function is called to give back the string that needs
 
 
 async def postprocess(generated, old):
-  if time.postprocess is True:
+  if time.postprocess:
     if generated < Config.script.max_length:
       return ""
 
